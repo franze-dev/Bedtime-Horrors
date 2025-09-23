f3798ceb6f01a8804e88c508a6bd1a8fdc1456cf
Nicolas Leon <nicolasaleon05@gmail.com>
2025-09-22 22:06:17 -0300

    Fix camera shake
    Fix problem where the earthquake camera shake wouldn't happen on build

---
+++ Assets/Scripts/Other/EditorCamera.cs
@@ -0,0 +1,16 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+#if UNITY_EDITOR
+public class EditorCamera : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    private void Awake()
+    {
+        _camera.gameObject.SetActive(true);
+        Camera.SetupCurrent(_camera);
+    }
+}
+
+#endif
