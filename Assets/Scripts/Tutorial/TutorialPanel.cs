553a6e43dd3b37dd8f260352dd64a07f1352115a
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-24 11:53:02 -0300

    Add focus points to tutorial

--- Assets/Scripts/Tutorial/TutorialPanel.cs
+++ Assets/Scripts/Tutorial/TutorialPanel.cs
@@ -1,17 +1,39 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialPanel : MonoBehaviour
 {
     [SerializeField] private GameObject _handGO;
-    
+    [SerializeField] private GameObject _focusBackground;
+    [SerializeField] private GameObject _focusPointsGO;
+
     private void OnEnable()
     {
         ShowHand(true);
+
+        ShowFocus(true);
     }
 
     private void OnDisable()
     {
         ShowHand(false);
+
+        ShowFocus(false);
+    }
+
+    private void ShowFocus(bool isFocus)
+    {
+        if (_focusBackground == null)
+            return;
+
+        _focusBackground.SetActive(isFocus);
+
+        if (_focusPointsGO == null)
+            return;
+
+        _focusPointsGO.SetActive(isFocus);
     }
 
     public void ShowHand(bool show)
