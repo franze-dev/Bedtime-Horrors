df461b32c7ab489ba3ebb1eb7ff76915a3ef816b
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-27 19:47:13 -0300

    Add area range setter

--- Assets/Scripts/Turrets/AreaNotifier.cs
+++ Assets/Scripts/Turrets/AreaNotifier.cs
@@ -2,11 +2,28 @@ using UnityEngine;
 
 public class AreaNotifier : MonoBehaviour
 {
+    [SerializeField] private float _spriteScale = 2f;
     private IAreaTurret _parentTurret;
+    private float _range;
+    private CircleCollider2D _circleCollider;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
-        _parentTurret = gameObject.GetComponentInParent<IAreaTurret>();
+        _parentTurret = GetComponentInParent<IAreaTurret>();
+        _circleCollider = GetComponent<CircleCollider2D>();
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+
+    }
+
+    public void SetRange(float range)
+    {
+        float scaledRange = range * _spriteScale;
+
+        _spriteRenderer.transform.localScale = new(scaledRange, scaledRange, scaledRange);
+        _circleCollider.radius = range;
+        _range = range;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
