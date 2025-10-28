bd89fa547520248aa3373f396386eb337326593a
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-28 20:22:08 -0300

    Fix sprites not switching when levelling up

--- Assets/Scripts/UI/LevelUpButton.cs
+++ Assets/Scripts/UI/LevelUpButton.cs
@@ -1,11 +1,34 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelUpButton : MonoBehaviour
 {
     [SerializeField] Turret _turret;
+    [SerializeField] List<GameObject> _buttonsGO;
+    [SerializeField] private int _firstButtonId = 0;
+    private int _currentButtonId = 0;
+
+    private void Awake()
+    {
+        foreach (var button in _buttonsGO)
+        {
+            button.SetActive(false);
+        }
+
+        ActivateButton(_firstButtonId);
+    }
+
+    private void ActivateButton(int id)
+    {
+        _currentButtonId = id;
+
+        _buttonsGO[_currentButtonId].SetActive(true);
+    }
 
     public void LevelUp()
     {
+        ActivateButton(_currentButtonId + 1);
         _turret.Upgrade();
         EventTriggerer.Trigger<ILevelUpEvent>(new LevelUpEvent());
     }
