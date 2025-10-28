cff14f18383ca70eaa07f2fd54720686ef68a8ea
sofi <genuinedistinctclearcapital@gmail.com>
2025-10-28 18:14:12 -0300

    Use menu state machine

--- Assets/Scripts/Scene Navigation/IMenuState.cs
+++ Assets/Scripts/Scene Navigation/IMenuState.cs
@@ -1,13 +1,20 @@
-﻿public interface IMenuState
+﻿
+public interface IMenuState
 {
     public void Enter(NavigationController controller);
+    public void Exit(NavigationController controller);
 }
 
 public class MainMenuState : IMenuState
 {
     public void Enter(NavigationController controller)
     {
-        controller.ShowMenu(controller.mainMenuGO);
+        controller.ShowMenu(controller.mainMenuGO, this);
+    }
+
+    public void Exit(NavigationController controller)
+    {
+        controller.HideMenu(controller.mainMenuGO);
     }
 }
 
@@ -15,7 +22,12 @@ public class WinMenuState : IMenuState
 {
     public void Enter(NavigationController controller)
     {
-        controller.ShowMenu(controller.winMenuGO);
+        controller.ShowMenu(controller.winMenuGO, this);
+    }
+
+    public void Exit(NavigationController controller)
+    {
+        controller.HideMenu(controller.winMenuGO);
     }
 }
 
@@ -23,7 +35,12 @@ public class LoseMenuState : IMenuState
 {
     public void Enter(NavigationController controller)
     {
-        controller.ShowMenu(controller.loseMenuGO);
+        controller.ShowMenu(controller.loseMenuGO, this);
+    }
+
+    public void Exit(NavigationController controller)
+    {
+        controller.HideMenu(controller.loseMenuGO);
     }
 }
 
@@ -31,7 +48,12 @@ public class SettingsMenuState : IMenuState
 {
     public void Enter(NavigationController controller)
     {
-        controller.ShowMenu(controller.settingsMenuGO);
+        controller.ShowMenu(controller.settingsMenuGO, this);
+    }
+
+    public void Exit(NavigationController controller)
+    {
+        controller.HideMenu(controller.settingsMenuGO);
     }
 }
 
@@ -39,7 +61,37 @@ public class CreditsMenuState : IMenuState
 {
     public void Enter(NavigationController controller)
     {
-        controller.ShowMenu(controller.creditsMenuGO);
+        controller.ShowMenu(controller.creditsMenuGO, this);
+    }
+
+    public void Exit(NavigationController controller)
+    {
+        controller.HideMenu(controller.creditsMenuGO);
     }
 }
 
+public class DiaryMenuState : IMenuState
+{
+    public void Enter(NavigationController controller)
+    {
+        controller.ShowMenu(controller.diaryMenuGO, this);
+    }
+
+    public void Exit(NavigationController controller)
+    {
+        controller.HideMenu(controller.diaryMenuGO);
+    }
+}
+
+public class PauseMenuState : IMenuState
+{
+    public void Enter(NavigationController controller)
+    {
+        controller.ShowMenu(controller.pauseMenuGO, this);
+    }
+
+    public void Exit(NavigationController controller)
+    {
+        controller.HideMenu(controller.pauseMenuGO);
+    }
+}
